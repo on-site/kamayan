a53b58e9a1c6343fafc0ee86c25bbbf5da649429
Mike Virata-Stone <mjstone@on-site.com>
2016-11-03 15:03:10 -0700

    Tweak ascii art a tad

--- course_01_collections/serving_05_stacks.rb
+++ course_01_collections/serving_05_stacks.rb
@@ -2,29 +2,27 @@ require_relative "../kamayan"
 
 # Diagram of a stack as you run operations on it:
 #
-# Empty stack  push(a)  push(b)  push(c)
-#
-#                                +---+
-#                                | c |
-#                       +---+    +---+
-#                       | b |    | b |
-#              +---+    +---+    +---+
-#              | a |    | a |    | a |
-# +===+        +---+    +---+    +---+
-#
-#
+#   Empty    push(a)  push(b)  push(c)
+#   Stack
+#                              +---+
+#                              | c |
+#                     +---+    +---+
+#                     | b |    | b |
+#            +---+    +---+    +---+
+#            | a |    | a |    | a |
+#   +===+    +---+    +---+    +---+
 #
 #
-#   peek()   pop()   peek()   pop()   pop()
-#   => c     => c    => b     => b    => a
+#   peek()   pop()    peek()   pop()    pop()
+#   => c     => c     => b     => b     => a
 #
 #   +---+
 #   | c |
-#   +---+    +---+   +---+
-#   | b |    | b |   | b |
-#   +---+    +---+   +---+    +---+
-#   | a |    | a |   | a |    | a |
-#   +---+    +---+   +---+    +---+   +===+
+#   +---+    +---+    +---+
+#   | b |    | b |    | b |
+#   +---+    +---+    +---+    +---+
+#   | a |    | a |    | a |    | a |
+#   +---+    +---+    +---+    +---+    +===+
 #
 class Serving05Stacks < Attestify::Test
 end
