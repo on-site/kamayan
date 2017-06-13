7a502ce5976ddbf6fb63fce49a3b2e77c642470d
Mike Virata-Stone <mjstone@on-site.com>
2017-06-13 16:54:21 -0700

    Fix Kamayan.send

--- java/src/main/java/com/on_site/kamayan/Kamayan.java
+++ java/src/main/java/com/on_site/kamayan/Kamayan.java
@@ -69,13 +69,51 @@ public class Kamayan {
 
     public static <T> T send(Object object, Class<T> expectedReturnType, String methodName, Object... params) {
         try {
-            Method method = object.getClass().getMethod(methodName, Arrays.stream(params).map(Object::getClass).collect(Collectors.toList()).toArray(new Class<?>[0]));
+            Method method = getMethod(object.getClass(), methodName, params);
             return expectedReturnType.cast(method.invoke(object, params));
         } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Method getMethod(Class<?> type, String methodName, Object... params) throws NoSuchMethodException {
+        Class<?>[] paramTypes = Arrays.stream(params).map(Object::getClass).collect(Collectors.toList()).toArray(new Class<?>[0]);
+
+        try {
+            return type.getMethod(methodName, paramTypes);
+        } catch (NoSuchMethodException e) {
+            Method[] methods = type.getMethods();
+
+            for (Method method : methods) {
+                if (methodMatches(method, methodName, paramTypes)) {
+                    return method;
+                }
+            }
+
+            throw e;
+        }
+    }
+
+    private static boolean methodMatches(Method method, String name, Class<?>... paramTypes) {
+        if (!method.getName().equals(name)) {
+            return false;
+        }
+
+        Class<?>[] expectedTypes = method.getParameterTypes();
+
+        if (expectedTypes.length != paramTypes.length) {
+            return false;
+        }
+
+        for (int i = 0; i < paramTypes.length; i++) {
+            if (!expectedTypes[i].isAssignableFrom(paramTypes[i])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void times(int n, Consumer<Integer> fn) {
